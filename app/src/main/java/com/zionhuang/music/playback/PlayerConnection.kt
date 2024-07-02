f72d6d5ca006c4ca8c9f0e96d256003677617cce
Amandeep singh <amandeep.singh.ccc@gmail.com>
2024-07-02 12:41:45 +0530

    Fix next song not playing automatically if previous song is paused

--- app/src/main/java/com/zionhuang/music/playback/PlayerConnection.kt
+++ app/src/main/java/com/zionhuang/music/playback/PlayerConnection.kt
@@ -124,6 +124,11 @@ class PlayerConnection(
         service.toggleLibrary()
     }
 
+    fun seekToNext(){
+        player.seekToNext()
+        player.playWhenReady = true
+    }
+
     override fun onPlaybackStateChanged(state: Int) {
         playbackState.value = state
         error.value = player.playerError

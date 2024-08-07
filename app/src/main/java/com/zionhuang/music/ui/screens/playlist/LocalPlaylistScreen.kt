a542b82f76bf52568b6cc347d551aeff8b3093c3
Zion Huang <zionhuang1107@gmail.com>
2024-08-07 15:28:17 +0800

    Add "Remove from playlist" to menu

--- app/src/main/java/com/zionhuang/music/ui/screens/playlist/LocalPlaylistScreen.kt
+++ app/src/main/java/com/zionhuang/music/ui/screens/playlist/LocalPlaylistScreen.kt
@@ -538,30 +538,35 @@ fun LocalPlaylistScreen(
                     key = song.map.id
                 ) {
                     val currentItem by rememberUpdatedState(song)
+
+                    fun deleteFromPlaylist() {
+                        database.transaction {
+                            move(currentItem.map.playlistId, currentItem.map.position, Int.MAX_VALUE)
+                            delete(currentItem.map.copy(position = Int.MAX_VALUE))
+                        }
+                        dismissJob?.cancel()
+                        dismissJob = coroutineScope.launch {
+                            val snackbarResult = snackbarHostState.showSnackbar(
+                                message = context.getString(R.string.removed_song_from_playlist, currentItem.song.song.title),
+                                actionLabel = context.getString(R.string.undo),
+                                duration = SnackbarDuration.Short
+                            )
+                            if (snackbarResult == SnackbarResult.ActionPerformed) {
+                                database.transaction {
+                                    insert(currentItem.map.copy(position = playlistLength))
+                                    move(currentItem.map.playlistId, playlistLength, currentItem.map.position)
+                                }
+                            }
+                        }
+                    }
+
                     val dismissState = rememberDismissState(
                         positionalThreshold = { totalDistance ->
                             totalDistance
                         },
                         confirmValueChange = { dismissValue ->
                             if (dismissValue == DismissValue.DismissedToEnd || dismissValue == DismissValue.DismissedToStart) {
-                                database.transaction {
-                                    move(currentItem.map.playlistId, currentItem.map.position, Int.MAX_VALUE)
-                                    delete(currentItem.map.copy(position = Int.MAX_VALUE))
-                                }
-                                dismissJob?.cancel()
-                                dismissJob = coroutineScope.launch {
-                                    val snackbarResult = snackbarHostState.showSnackbar(
-                                        message = context.getString(R.string.removed_song_from_playlist, currentItem.song.song.title),
-                                        actionLabel = context.getString(R.string.undo),
-                                        duration = SnackbarDuration.Short
-                                    )
-                                    if (snackbarResult == SnackbarResult.ActionPerformed) {
-                                        database.transaction {
-                                            insert(currentItem.map.copy(position = playlistLength))
-                                            move(currentItem.map.playlistId, playlistLength, currentItem.map.position)
-                                        }
-                                    }
-                                }
+                                deleteFromPlaylist()
                             }
                             true
                         }
@@ -580,7 +585,8 @@ fun LocalPlaylistScreen(
                                             SongMenu(
                                                 originalSong = song.song,
                                                 navController = navController,
-                                                onDismiss = menuState::dismiss
+                                                onDismiss = menuState::dismiss,
+                                                onDeleteFromPlaylist = ::deleteFromPlaylist
                                             )
                                         }
                                     }

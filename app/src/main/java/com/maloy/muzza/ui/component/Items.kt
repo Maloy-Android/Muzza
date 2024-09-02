c56bfb749ba292f9603cac303ba1a1fc21b5377a
Maloy <108239845+MaloyBegonia@users.noreply.github.com>
2024-09-02 14:39:14 +0300

    fix: grid item thumbnail alignment

--- app/src/main/java/com/maloy/muzza/ui/component/Items.kt
+++ app/src/main/java/com/maloy/muzza/ui/component/Items.kt
@@ -195,6 +195,7 @@ fun GridItem(
         }
     ) {
         BoxWithConstraints(
+            contentAlignment = Alignment.Center,
             modifier = if (fillMaxWidth) {
                 Modifier.fillMaxWidth()
             } else {

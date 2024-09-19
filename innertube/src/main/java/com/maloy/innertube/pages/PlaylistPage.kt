da0f08eccd7b6878b51bf59bb47e4d32b8a44c4a
Maloy <108239845+MaloyBegonia@users.noreply.github.com>
2024-09-19 20:41:10 +0300

    fix: not showing artists on collaborators playlists

--- innertube/src/main/java/com/maloy/innertube/pages/PlaylistPage.kt
+++ innertube/src/main/java/com/maloy/innertube/pages/PlaylistPage.kt
@@ -16,16 +16,41 @@ data class PlaylistPage(
 ) {
     companion object {
         fun fromMusicResponsiveListItemRenderer(renderer: MusicResponsiveListItemRenderer): SongItem? {
+            val artists: List<Artist>? =
+                renderer.flexColumns
+                    .getOrNull(1)
+                    ?.musicResponsiveListItemFlexColumnRenderer
+                    ?.text
+                    ?.runs
+                    ?.oddElements()
+                    ?.map {
+                        Artist(
+                            name = it.text,
+                            id = it.navigationEndpoint?.browseEndpoint?.browseId,
+                        )
+                    }
+            val collaborators: List<Artist>? =
+                renderer.flexColumns
+                    .getOrNull(2)
+                    ?.musicResponsiveListItemFlexColumnRenderer
+                    ?.text
+                    ?.runs
+                    ?.oddElements()
+                    ?.map {
+                        Artist(
+                            name = it.text,
+                            id = it.navigationEndpoint?.browseEndpoint?.browseId,
+                        )
+                    }
             return SongItem(
                 id = renderer.playlistItemData?.videoId ?: return null,
                 title = renderer.flexColumns.firstOrNull()
                     ?.musicResponsiveListItemFlexColumnRenderer?.text
                     ?.runs?.firstOrNull()?.text ?: return null,
-                artists = renderer.flexColumns.getOrNull(1)?.musicResponsiveListItemFlexColumnRenderer?.text?.runs?.oddElements()?.map {
-                    Artist(
-                        name = it.text,
-                        id = it.navigationEndpoint?.browseEndpoint?.browseId,
-                    )
+                artists =                     if (collaborators != null) {
+                    artists!!.plus(collaborators)
+                } else {
+                    artists
                 }.orEmpty().ifEmpty { return null },
                 album = renderer.flexColumns.getOrNull(2)?.musicResponsiveListItemFlexColumnRenderer?.text?.runs?.firstOrNull()?.let {
                     Album(

9ce6d58db4fdc27567d22ec10bdb1f6cf767311a
Maloy <108239845+MaloyBegonia@users.noreply.github.com>
2024-09-21 22:27:15 +0300

    Revert da0f08eccd7b6878b51bf59bb47e4d32b8a44c4a

--- innertube/src/main/java/com/maloy/innertube/pages/PlaylistPage.kt
+++ innertube/src/main/java/com/maloy/innertube/pages/PlaylistPage.kt
@@ -16,41 +16,16 @@ data class PlaylistPage(
 ) {
     companion object {
         fun fromMusicResponsiveListItemRenderer(renderer: MusicResponsiveListItemRenderer): SongItem? {
-            val artists: List<Artist>? =
-                renderer.flexColumns
-                    .getOrNull(1)
-                    ?.musicResponsiveListItemFlexColumnRenderer
-                    ?.text
-                    ?.runs
-                    ?.oddElements()
-                    ?.map {
-                        Artist(
-                            name = it.text,
-                            id = it.navigationEndpoint?.browseEndpoint?.browseId,
-                        )
-                    }
-            val collaborators: List<Artist>? =
-                renderer.flexColumns
-                    .getOrNull(2)
-                    ?.musicResponsiveListItemFlexColumnRenderer
-                    ?.text
-                    ?.runs
-                    ?.oddElements()
-                    ?.map {
-                        Artist(
-                            name = it.text,
-                            id = it.navigationEndpoint?.browseEndpoint?.browseId,
-                        )
-                    }
             return SongItem(
                 id = renderer.playlistItemData?.videoId ?: return null,
                 title = renderer.flexColumns.firstOrNull()
                     ?.musicResponsiveListItemFlexColumnRenderer?.text
                     ?.runs?.firstOrNull()?.text ?: return null,
-                artists =                     if (collaborators != null) {
-                    artists!!.plus(collaborators)
-                } else {
-                    artists
+                artists = renderer.flexColumns.getOrNull(1)?.musicResponsiveListItemFlexColumnRenderer?.text?.runs?.oddElements()?.map {
+                    Artist(
+                        name = it.text,
+                        id = it.navigationEndpoint?.browseEndpoint?.browseId,
+                    )
                 }.orEmpty().ifEmpty { return null },
                 album = renderer.flexColumns.getOrNull(2)?.musicResponsiveListItemFlexColumnRenderer?.text?.runs?.firstOrNull()?.let {
                     Album(

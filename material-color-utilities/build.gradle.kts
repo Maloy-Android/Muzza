81052c719d2f21d228c397f5b4e92b5c79cca13d
Maloy <108239845+MaloyBegonia@users.noreply.github.com>
2024-09-23 18:51:06 +0300

    Update gradle and libs

--- material-color-utilities/build.gradle.kts
+++ material-color-utilities/build.gradle.kts
@@ -8,6 +8,6 @@ java {
 }
 
 dependencies {
-    compileOnly("com.google.errorprone:error_prone_core:2.16")
+    compileOnly("com.google.errorprone:error_prone_core:2.28.0")
     implementation(libs.annotation)
 }

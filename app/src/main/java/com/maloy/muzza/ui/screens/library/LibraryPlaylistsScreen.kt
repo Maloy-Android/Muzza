6f02f6b8c62665073e83b2efcd9461de425bdf0d
Maloy <108239845+MaloyBegonia@users.noreply.github.com>
2024-11-04 16:31:42 +0300

    Feat(ui): redesign of Library

--- app/src/main/java/com/maloy/muzza/ui/screens/library/LibraryPlaylistsScreen.kt
+++ app/src/main/java/com/maloy/muzza/ui/screens/library/LibraryPlaylistsScreen.kt
@@ -85,6 +85,7 @@ import java.util.UUID
 @Composable
 fun LibraryPlaylistsScreen(
     navController: NavController,
+    filterContent: @Composable () -> Unit,
     viewModel: LibraryPlaylistsViewModel = hiltViewModel(),
 ) {
     val menuState = LocalMenuState.current
@@ -249,6 +250,12 @@ fun LibraryPlaylistsScreen(
                     contentPadding = LocalPlayerAwareWindowInsets.current.asPaddingValues()
                 ) {
                     item(
+                        key = "filter",
+                        contentType = CONTENT_TYPE_HEADER
+                    ) {
+                        filterContent()
+                    }
+                    item(
                         key = "header",
                         contentType = CONTENT_TYPE_HEADER
                     ) {
@@ -396,6 +403,13 @@ fun LibraryPlaylistsScreen(
                     ),
                     contentPadding = LocalPlayerAwareWindowInsets.current.asPaddingValues()
                 ) {
+                    item(
+                        key = "filter",
+                        span = { GridItemSpan(maxLineSpan) },
+                        contentType = CONTENT_TYPE_HEADER
+                    ) {
+                        filterContent()
+                    }
                     item(
                         key = "header",
                         span = { GridItemSpan(maxLineSpan) },

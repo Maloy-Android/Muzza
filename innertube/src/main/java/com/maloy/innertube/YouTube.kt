584fe6f8dab7f57a6236afa2f593b77721fa76ce
Maloy <108239845+MaloyBegonia@users.noreply.github.com>
2025-01-22 21:17:39 +0300

    Fix:Playing error (final)

--- innertube/src/main/java/com/maloy/innertube/YouTube.kt
+++ innertube/src/main/java/com/maloy/innertube/YouTube.kt
@@ -13,6 +13,7 @@ import com.maloy.innertube.models.SearchSuggestions
 import com.maloy.innertube.models.SongItem
 import com.maloy.innertube.models.WatchEndpoint
 import com.maloy.innertube.models.WatchEndpoint.WatchEndpointMusicSupportedConfigs.WatchEndpointMusicConfig.Companion.MUSIC_VIDEO_TYPE_ATV
+import com.maloy.innertube.models.YouTubeClient.Companion.ANDROID_MUSIC
 import com.maloy.innertube.models.YouTubeClient.Companion.IOS
 import com.maloy.innertube.models.YouTubeClient.Companion.TVHTML5
 import com.maloy.innertube.models.YouTubeClient.Companion.WEB
@@ -520,7 +521,14 @@ val response = innerTube.browse(WEB_REMIX, continuation = continuation).body<Bro
     }
 
     suspend fun player(videoId: String, playlistId: String? = null): Result<PlayerResponse> = runCatching {
-        val playerResponse: PlayerResponse = innerTube.player(IOS, videoId, playlistId).body<PlayerResponse>()
+        var playerResponse: PlayerResponse
+        if (this.cookie != null) { // if logged in: try ANDROID_MUSIC client first because IOS client does not support login
+            playerResponse = innerTube.player(ANDROID_MUSIC, videoId, playlistId).body<PlayerResponse>()
+            if (playerResponse.playabilityStatus.status == "OK") {
+                return@runCatching playerResponse
+            }
+        }
+        playerResponse = innerTube.player(IOS, videoId, playlistId).body<PlayerResponse>()
         if (playerResponse.playabilityStatus.status == "OK") {
             return@runCatching playerResponse
         }

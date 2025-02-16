7647a7625f251d10a041e5d143eba43ad6d9ee92
Maloy <108239845+MaloyBegonia@users.noreply.github.com>
2025-02-16 23:01:12 +0300

    Feat(PlayerError):Correct colour for player error text

--- app/src/main/java/com/maloy/muzza/ui/player/PlaybackError.kt
+++ app/src/main/java/com/maloy/muzza/ui/player/PlaybackError.kt
@@ -1,12 +1,15 @@
 package com.maloy.muzza.ui.player
 
 import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
@@ -15,12 +18,31 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.media3.common.PlaybackException
 import com.maloy.muzza.R
+import com.maloy.muzza.constants.DarkModeKey
+import com.maloy.muzza.constants.PlayerBackgroundStyle
+import com.maloy.muzza.constants.PlayerBackgroundStyleKey
+import com.maloy.muzza.ui.screens.settings.DarkMode
+import com.maloy.muzza.utils.rememberEnumPreference
 
 @Composable
 fun PlaybackError(
     error: PlaybackException,
     retry: () -> Unit,
 ) {
+    val playerBackground by rememberEnumPreference(key = PlayerBackgroundStyleKey, defaultValue = PlayerBackgroundStyle.DEFAULT)
+    val darkTheme by rememberEnumPreference(DarkModeKey, defaultValue = DarkMode.AUTO)
+    val isSystemInDarkTheme = isSystemInDarkTheme()
+    val useDarkTheme = remember(darkTheme, isSystemInDarkTheme) {
+        if (darkTheme == DarkMode.AUTO) isSystemInDarkTheme else darkTheme == DarkMode.ON
+    }
+    val textColor = when (playerBackground) {
+        PlayerBackgroundStyle.DEFAULT -> MaterialTheme.colorScheme.secondary
+        else ->
+            if (useDarkTheme)
+                MaterialTheme.colorScheme.onSurface
+            else
+                MaterialTheme.colorScheme.onPrimary
+    }
     Row(
         horizontalArrangement = Arrangement.spacedBy(12.dp),
         verticalAlignment = Alignment.CenterVertically,
@@ -38,6 +60,7 @@ fun PlaybackError(
 
         Text(
             text = error.cause?.cause?.message ?: stringResource(R.string.error_unknown),
+            color = textColor,
             style = MaterialTheme.typography.bodyMedium
         )
     }

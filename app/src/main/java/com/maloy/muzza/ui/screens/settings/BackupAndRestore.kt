91680102c337330f5672b3084789ce8c85d655f7
Maxim <msermavbrin69@gmail.com>
2025-05-08 14:00:42 +0300

    Feat(BackupRestore,UI):Refactor backup, restore in cardItem

--- app/src/main/java/com/maloy/muzza/ui/screens/settings/BackupAndRestore.kt
+++ app/src/main/java/com/maloy/muzza/ui/screens/settings/BackupAndRestore.kt
@@ -3,28 +3,40 @@ package com.maloy.muzza.ui.screens.settings
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.only
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.maloy.muzza.LocalPlayerAwareWindowInsets
 import com.maloy.muzza.R
 import com.maloy.muzza.ui.component.IconButton
-import com.maloy.muzza.ui.component.PreferenceEntry
 import com.maloy.muzza.ui.utils.backToMain
 import com.maloy.muzza.viewmodels.BackupRestoreViewModel
 import java.time.LocalDateTime
@@ -56,21 +68,26 @@ fun BackupAndRestore(
     ) {
         Spacer(Modifier.windowInsetsPadding(LocalPlayerAwareWindowInsets.current.only(WindowInsetsSides.Top)))
 
-        PreferenceEntry(
-            title = { Text(stringResource(R.string.backup)) },
-            icon = { Icon(painterResource(R.drawable.backup), null) },
+        Spacer(Modifier.height(20.dp))
+
+        CardItemBackupRestore(
+            icon = R.drawable.backup,
+            title = stringResource(R.string.backup),
             onClick = {
                 val formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss")
                 backupLauncher.launch("${context.getString(R.string.app_name)}_${LocalDateTime.now().format(formatter)}.backup")
             }
         )
-        PreferenceEntry(
-            title = { Text(stringResource(R.string.restore)) },
-            icon = { Icon(painterResource(R.drawable.restore), null) },
-            onClick = {
-                restoreLauncher.launch(arrayOf("application/octet-stream"))
-            }
+
+        Spacer(Modifier.height(20.dp))
+
+        CardItemBackupRestore(
+            icon = R.drawable.restore,
+            title = stringResource(R.string.backup_restore),
+            onClick = { restoreLauncher.launch(arrayOf("application/octet-stream")) }
         )
+
+        Spacer(Modifier.height(20.dp))
     }
 
     TopAppBar(
@@ -88,4 +105,45 @@ fun BackupAndRestore(
         },
         scrollBehavior = scrollBehavior
     )
+}
+
+@Composable
+fun CardItemBackupRestore(
+    icon: Int,
+    title: String,
+    onClick: () -> Unit
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(120.dp)
+            .padding(horizontal = 16.dp),
+        shape = RoundedCornerShape(20.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+        ),
+        onClick = onClick
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                painter = painterResource(icon),
+                contentDescription = null,
+                modifier = Modifier.size(28.dp),
+                tint = MaterialTheme.colorScheme.primary
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Column {
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            }
+        }
+    }
 }

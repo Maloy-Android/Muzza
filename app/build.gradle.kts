1b9cd1b245b7108482898009c2c851608ecd83b9
Maxim <msermavbrin69@gmail.com>
2025-05-11 21:15:30 +0300

    Bump to "0.6.9"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.maloy.muzza"
         minSdk = 24
         targetSdk = 35
-        versionCode = 39
-        versionName = "0.6.8"
+        versionCode = 40
+        versionName = "0.6.9"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {

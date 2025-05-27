c60559936816571a93e15b5c5177b6d2b5e9a36d
Maxim <msermavbrin69@gmail.com>
2025-05-27 10:50:55 +0300

    Feat(BackupAndRestore):Import playlist (m3u)

--- app/src/main/java/com/maloy/muzza/ui/screens/settings/BackupAndRestore.kt
+++ app/src/main/java/com/maloy/muzza/ui/screens/settings/BackupAndRestore.kt
@@ -9,11 +9,14 @@ import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -26,6 +29,12 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -36,8 +45,13 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.maloy.muzza.LocalPlayerAwareWindowInsets
 import com.maloy.muzza.R
+import com.maloy.muzza.constants.ScannerSensitivity
+import com.maloy.muzza.constants.ScannerSensitivityKey
+import com.maloy.muzza.db.entities.Song
 import com.maloy.muzza.ui.component.IconButton
+import com.maloy.muzza.ui.menu.AddToPlaylistDialog
 import com.maloy.muzza.ui.utils.backToMain
+import com.maloy.muzza.utils.rememberEnumPreference
 import com.maloy.muzza.viewmodels.BackupRestoreViewModel
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -60,13 +74,43 @@ fun BackupAndRestore(
             viewModel.restore(context, uri)
         }
     }
+    val (scannerSensitivity) = rememberEnumPreference(
+        key = ScannerSensitivityKey,
+        defaultValue = ScannerSensitivity.LEVEL_2
+    )
+
+    var showChoosePlaylistDialog by rememberSaveable {
+        mutableStateOf(false)
+    }
+    val importedTitle by remember { mutableStateOf("") }
+    val importedSongs = remember { mutableStateListOf<Song>() }
+    val rejectedSongs = remember { mutableStateListOf<String>() }
+    val importM3uLauncher = rememberLauncherForActivityResult(ActivityResultContracts.OpenDocument()) { uri ->
+        if (uri != null) {
+            val result = viewModel.loadM3u(context, uri, matchStrength = scannerSensitivity)
+            importedSongs.clear()
+            importedSongs.addAll(result.first)
+            rejectedSongs.clear()
+            rejectedSongs.addAll(result.second)
+
+            if (importedSongs.isNotEmpty()) {
+                showChoosePlaylistDialog = true
+            }
+        }
+    }
 
     Column(
         Modifier
             .windowInsetsPadding(LocalPlayerAwareWindowInsets.current.only(WindowInsetsSides.Horizontal + WindowInsetsSides.Bottom))
             .verticalScroll(rememberScrollState())
     ) {
-        Spacer(Modifier.windowInsetsPadding(LocalPlayerAwareWindowInsets.current.only(WindowInsetsSides.Top)))
+        Spacer(
+            Modifier.windowInsetsPadding(
+                LocalPlayerAwareWindowInsets.current.only(
+                    WindowInsetsSides.Top
+                )
+            )
+        )
 
         Spacer(Modifier.height(20.dp))
 
@@ -75,7 +119,11 @@ fun BackupAndRestore(
             title = stringResource(R.string.backup),
             onClick = {
                 val formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss")
-                backupLauncher.launch("${context.getString(R.string.app_name)}_${LocalDateTime.now().format(formatter)}.backup")
+                backupLauncher.launch(
+                    "${context.getString(R.string.app_name)}_${
+                        LocalDateTime.now().format(formatter)
+                    }.backup"
+                )
             }
         )
 
@@ -88,6 +136,47 @@ fun BackupAndRestore(
         )
 
         Spacer(Modifier.height(20.dp))
+
+        CardItemBackupRestore(
+            icon = R.drawable.playlist_add,
+            title = stringResource(R.string.import_m3u),
+            onClick = {
+                importM3uLauncher.launch(arrayOf("audio/*"))
+            }
+        )
+        AddToPlaylistDialog(
+            isVisible = showChoosePlaylistDialog,
+            initialTextFieldValue = importedTitle,
+            onGetSong = { importedSongs.map { it.id } },
+            onDismiss = { showChoosePlaylistDialog = false }
+        )
+
+        if (rejectedSongs.isNotEmpty()) {
+            LazyColumn(
+                modifier = Modifier
+                    .heightIn(max = 250.dp)
+                    .padding(20.dp)
+            ) {
+                item {
+                    Text(
+                        text = "Could not import:",
+                        maxLines = 1,
+                    )
+                }
+
+                itemsIndexed(
+                    items = rejectedSongs,
+                    key = { _, song -> song.hashCode() }
+                ) { _, item ->
+                    Text(
+                        text = item,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 8.dp)
+                    )
+                }
+            }
+        }
     }
 
     TopAppBar(

5bcd39d1daf4c107604f616fc2ce6fb097df8500
Maxim <msermavbrin69@gmail.com>
2025-06-17 21:23:20 +0300

    Feat(HistoryPage):Bug fix

--- innertube/src/main/java/com/maloy/innertube/pages/HistoryPage.kt
+++ innertube/src/main/java/com/maloy/innertube/pages/HistoryPage.kt
@@ -6,6 +6,8 @@ import com.maloy.innertube.models.MusicResponsiveListItemRenderer
 import com.maloy.innertube.models.MusicShelfRenderer
 import com.maloy.innertube.models.SongItem
 import com.maloy.innertube.models.getItems
+import com.maloy.innertube.models.oddElements
+import com.maloy.innertube.utils.parseTime
 
 data class HistoryPage(
     val sections: List<HistorySection>?,
@@ -26,34 +28,29 @@ data class HistoryPage(
         private fun fromMusicResponsiveListItemRenderer(renderer: MusicResponsiveListItemRenderer): SongItem? {
             return SongItem(
                 id = renderer.playlistItemData?.videoId ?: return null,
-                title = renderer.flexColumns.firstOrNull()?.musicResponsiveListItemFlexColumnRenderer?.text?.runs?.firstOrNull()?.text?: return null,
-                artists = renderer.flexColumns.getOrNull(1)
-                    ?.musicResponsiveListItemFlexColumnRenderer?.text?.runs
-                    ?.mapNotNull { it ->
-                        it.takeIf { run ->
-                            run.navigationEndpoint?.browseEndpoint?.browseId != null
-                        }?.let {
-                            Artist(
-                                name = it.text,
-                                id = it.navigationEndpoint?.browseEndpoint?.browseId
-                            )
-                        }
-                    } ?: emptyList(),
-                album = renderer.flexColumns.getOrNull(2)?.musicResponsiveListItemFlexColumnRenderer?.text?.runs?.firstOrNull()
-                    ?.takeIf {
-                        it.navigationEndpoint?.browseEndpoint?.browseId != null
-                    }?.let {
-                        it.navigationEndpoint?.browseEndpoint?.browseId?.let { it1 ->
-                            Album(
-                                name = it.text, id = it1
-                            )
-                        }
-                    },
-                duration = null,
+                title = renderer.flexColumns.firstOrNull()
+                    ?.musicResponsiveListItemFlexColumnRenderer?.text?.runs?.firstOrNull()
+                    ?.text ?: return null,
+                artists = renderer.flexColumns.getOrNull(1)?.musicResponsiveListItemFlexColumnRenderer?.text?.runs?.oddElements()?.map {
+                    Artist(
+                        name = it.text,
+                        id = it.navigationEndpoint?.browseEndpoint?.browseId
+                    )
+                } ?: emptyList(),
+                album = renderer.flexColumns.getOrNull(3)?.musicResponsiveListItemFlexColumnRenderer?.text?.runs?.firstOrNull()?.let {
+                    Album(
+                        name = it.text,
+                        id = it.navigationEndpoint?.browseEndpoint?.browseId ?: return@let null
+                    )
+                },
+                duration = renderer.fixedColumns?.firstOrNull()?.musicResponsiveListItemFlexColumnRenderer
+                    ?.text?.runs?.firstOrNull()?.text?.parseTime(),
                 thumbnail = renderer.thumbnail?.musicThumbnailRenderer?.getThumbnailUrl() ?: return null,
                 explicit = renderer.badges?.find {
                     it.musicInlineBadgeRenderer?.icon?.iconType == "MUSIC_EXPLICIT_BADGE"
-                } != null
+                } != null,
+                endpoint = renderer.overlay?.musicItemThumbnailOverlayRenderer?.content
+                    ?.musicPlayButtonRenderer?.playNavigationEndpoint?.watchEndpoint
             )
         }
     }

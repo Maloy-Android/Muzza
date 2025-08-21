8a1036e9f8fe4e98a5620947ac1943e0f6e86d4b
Maxim <msermavbrin69@gmail.com>
2025-08-21 14:00:41 +0300

    Feat(App):Using latest NewPipeExtractor by TeamNewPipe

--- innertube/src/main/java/com/maloy/innertube/pages/NewPipe.kt
+++ innertube/src/main/java/com/maloy/innertube/pages/NewPipe.kt
@@ -8,7 +8,6 @@ import io.ktor.http.parseQueryString
 import okhttp3.OkHttpClient
 import okhttp3.RequestBody.Companion.toRequestBody
 import org.schabi.newpipe.extractor.NewPipe
-import org.schabi.newpipe.extractor.downloader.CancellableCall
 import org.schabi.newpipe.extractor.downloader.Downloader
 import org.schabi.newpipe.extractor.downloader.Request
 import org.schabi.newpipe.extractor.downloader.Response
@@ -49,11 +48,7 @@ class NewPipeDownloaderImpl(proxy: Proxy?) : Downloader() {
         }
         val responseBodyToReturn = response.body?.string()
         val latestUrl = response.request.url.toString()
-        return Response(response.code, response.message, response.headers.toMultimap(), responseBodyToReturn, responseBodyToReturn?.toByteArray(), latestUrl)
-    }
-
-    override fun executeAsync(request: Request, callback: AsyncCallback?): CancellableCall {
-        TODO("Placeholder")
+        return Response(response.code, response.message, response.headers.toMultimap(), responseBodyToReturn, latestUrl)
     }
 }
 object NewPipeUtils {

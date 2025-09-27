e0e693f821dc72b913f31a6298912aad8d7bfc5d
Maxim <msermavbrin69@gmail.com>
2025-09-27 14:35:23 +0300

    Fixed(Searching,API):No results found when user logged

--- innertube/src/main/java/com/maloy/innertube/InnerTube.kt
+++ innertube/src/main/java/com/maloy/innertube/InnerTube.kt
@@ -207,7 +207,7 @@ class InnerTube {
         params: String? = null,
         continuation: String? = null,
     ) = httpClient.post("search") {
-        ytClient(client, setLogin = useLoginForBrowse)
+        ytClient(client, setLogin = false)
         setBody(
             SearchBody(
                 context = client.toContext(locale, visitorData, dataSyncId),

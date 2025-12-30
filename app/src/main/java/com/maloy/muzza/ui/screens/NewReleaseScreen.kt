4c3d2c9d22e6e09e6f0439698632ca3cc5cf4752
Maxim <msermavbrin69@gmail.com>
2025-12-30 23:58:13 +0300

    Feat(NewReleasesScreen):List view button for items

--- app/src/main/java/com/maloy/muzza/ui/screens/NewReleaseScreen.kt
+++ app/src/main/java/com/maloy/muzza/ui/screens/NewReleaseScreen.kt
@@ -7,10 +7,12 @@ import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
@@ -25,6 +27,7 @@ import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
@@ -37,15 +40,24 @@ import androidx.navigation.NavController
 import com.maloy.muzza.LocalPlayerAwareWindowInsets
 import com.maloy.muzza.LocalPlayerConnection
 import com.maloy.muzza.R
+import com.maloy.muzza.constants.AlbumViewTypeKey
+import com.maloy.muzza.constants.GridCellSize
+import com.maloy.muzza.constants.GridCellSizeKey
 import com.maloy.muzza.constants.GridThumbnailHeight
+import com.maloy.muzza.constants.LibraryViewType
+import com.maloy.muzza.constants.SmallGridThumbnailHeight
+import androidx.compose.foundation.lazy.items
 import com.maloy.muzza.ui.component.IconButton
+import com.maloy.muzza.ui.component.LazyColumnScrollbar
 import com.maloy.muzza.ui.component.LazyVerticalGridScrollbar
 import com.maloy.muzza.ui.component.LocalMenuState
 import com.maloy.muzza.ui.component.YouTubeGridItem
+import com.maloy.muzza.ui.component.YouTubeListItem
 import com.maloy.muzza.ui.component.shimmer.GridItemPlaceHolder
 import com.maloy.muzza.ui.component.shimmer.ShimmerHost
 import com.maloy.muzza.ui.menu.YouTubeAlbumMenu
 import com.maloy.muzza.ui.utils.backToMain
+import com.maloy.muzza.utils.rememberEnumPreference
 import com.maloy.muzza.viewmodels.NewReleaseViewModel
 
 @SuppressLint("UnusedBoxWithConstraintsScope")
@@ -69,6 +81,17 @@ fun NewReleaseScreen(
         }
     }
 
+    val lazyListState = rememberLazyListState()
+
+    val lazyChecker by remember {
+        derivedStateOf {
+            lazyListState.firstVisibleItemIndex > 0
+        }
+    }
+
+    val gridCellSize by rememberEnumPreference(GridCellSizeKey, GridCellSize.SMALL)
+    var viewType by rememberEnumPreference(AlbumViewTypeKey, LibraryViewType.GRID)
+
     val newReleaseAlbums by viewModel.newReleaseAlbums.collectAsState()
 
     val coroutineScope = rememberCoroutineScope()
@@ -87,48 +110,119 @@ fun NewReleaseScreen(
             ),
         contentAlignment = Alignment.TopStart
     ) {
-        LazyVerticalGrid(
-            columns = GridCells.Adaptive(minSize = GridThumbnailHeight + 24.dp),
-            contentPadding = LocalPlayerAwareWindowInsets.current.asPaddingValues(),
-            state = lazyGridState
-        ) {
-            items(
-                items = newReleaseAlbums,
-                key = { it.id }
-            ) { album ->
-                YouTubeGridItem(
-                    item = album,
-                    isActive = mediaMetadata?.album?.id == album.id,
-                    isPlaying = isPlaying,
-                    fillMaxWidth = true,
-                    coroutineScope = coroutineScope,
-                    modifier = Modifier
-                        .combinedClickable(
-                            onClick = {
-                                navController.navigate("album/${album.id}")
-                            },
-                            onLongClick = {
-                                haptic.performHapticFeedback(HapticFeedbackType.LongPress)
-                                menuState.show {
-                                    YouTubeAlbumMenu(
-                                        albumItem = album,
-                                        navController = navController,
-                                        onDismiss = menuState::dismiss
+        when (viewType) {
+            LibraryViewType.LIST -> {
+                LazyColumn(
+                    state = lazyListState,
+                    contentPadding = LocalPlayerAwareWindowInsets.current.asPaddingValues()
+                ) {
+                    items(
+                        items = newReleaseAlbums,
+                        key = { it.id }
+                    ) { album ->
+                        YouTubeListItem(
+                            item = album,
+                            isActive = mediaMetadata?.album?.id == album.id,
+                            isPlaying = isPlaying,
+                            trailingContent = {
+                                androidx.compose.material3.IconButton(
+                                    onClick = {
+                                            menuState.show {
+                                                YouTubeAlbumMenu(
+                                                    albumItem = album,
+                                                    navController = navController,
+                                                    onDismiss = menuState::dismiss
+                                                )
+                                            }
+                                    }
+                                ) {
+                                    Icon(
+                                        painter = painterResource(R.drawable.more_vert),
+                                        contentDescription = null
                                     )
                                 }
-                            }
+                            },
+                            modifier = Modifier
+                                .combinedClickable(
+                                    onClick = {
+                                        navController.navigate("album/${album.id}")
+                                    },
+                                    onLongClick = {
+                                        haptic.performHapticFeedback(HapticFeedbackType.LongPress)
+                                        menuState.show {
+                                            YouTubeAlbumMenu(
+                                                albumItem = album,
+                                                navController = navController,
+                                                onDismiss = menuState::dismiss
+                                            )
+                                        }
+                                    }
+                                )
                         )
-                )
+                    }
+                }
+                if (lazyChecker) {
+                    LazyColumnScrollbar(
+                        state = lazyListState
+                    )
+                }
             }
 
-            if (newReleaseAlbums.isEmpty()) {
-                items(8) {
-                    ShimmerHost {
-                        GridItemPlaceHolder(fillMaxWidth = true)
+            LibraryViewType.GRID -> {
+                LazyVerticalGrid(
+                    state = lazyGridState,
+                    columns = GridCells.Adaptive(
+                        minSize = when (gridCellSize) {
+                            GridCellSize.SMALL -> SmallGridThumbnailHeight
+                            GridCellSize.BIG -> GridThumbnailHeight
+                        } + 24.dp
+                    ),
+                    contentPadding = LocalPlayerAwareWindowInsets.current.asPaddingValues()
+                ) {
+                    items(
+                        items = newReleaseAlbums,
+                        key = { it.id }
+                    ) { album ->
+                        YouTubeGridItem(
+                            item = album,
+                            isActive = mediaMetadata?.album?.id == album.id,
+                            isPlaying = isPlaying,
+                            fillMaxWidth = true,
+                            coroutineScope = coroutineScope,
+                            modifier = Modifier
+                                .combinedClickable(
+                                    onClick = {
+                                        navController.navigate("album/${album.id}")
+                                    },
+                                    onLongClick = {
+                                        haptic.performHapticFeedback(HapticFeedbackType.LongPress)
+                                        menuState.show {
+                                            YouTubeAlbumMenu(
+                                                albumItem = album,
+                                                navController = navController,
+                                                onDismiss = menuState::dismiss
+                                            )
+                                        }
+                                    }
+                                )
+                        )
                     }
+                    if (newReleaseAlbums.isEmpty()) {
+                        items(8) {
+                            ShimmerHost {
+                                GridItemPlaceHolder(fillMaxWidth = true)
+                            }
+                        }
+                    }
+                }
+                if (gridChecker) {
+                    LazyVerticalGridScrollbar(
+                        state = lazyGridState
+                    )
                 }
             }
         }
+
         if (newReleaseAlbums.isNotEmpty()) {
             Indicator(
                 isRefreshing = isRefreshing,
@@ -138,12 +232,6 @@ fun NewReleaseScreen(
                     .padding(LocalPlayerAwareWindowInsets.current.asPaddingValues()),
             )
         }
-    }
-
-    if (gridChecker) {
-        LazyVerticalGridScrollbar(
-            state = lazyGridState
-        )
     }
 
     CenterAlignedTopAppBar(
@@ -157,6 +245,25 @@ fun NewReleaseScreen(
                     painterResource(R.drawable.arrow_back),
                     contentDescription = null
                 )
+            }
+        },
+        actions = {
+            if (newReleaseAlbums.isNotEmpty()) {
+                androidx.compose.material3.IconButton(
+                    onClick = {
+                        viewType = viewType.toggle()
+                    }
+                ) {
+                    Icon(
+                        painter = painterResource(
+                            when (viewType) {
+                                LibraryViewType.LIST -> R.drawable.list
+                                LibraryViewType.GRID -> R.drawable.grid_view
+                            }
+                        ),
+                        contentDescription = null
+                    )
+                }
             }
         },
         scrollBehavior = scrollBehavior

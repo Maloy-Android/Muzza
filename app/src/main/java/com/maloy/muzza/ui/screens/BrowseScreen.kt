8ff689e2f46d581672cd41a45924c263554bf4d3
Maksyushka <msermavbrin69@gmail.com>
2026-02-07 14:33:57 +0300

    Feat(BrowseScreen):TopAppBar -> CenterAlignedTopAppBar

--- app/src/main/java/com/maloy/muzza/ui/screens/BrowseScreen.kt
+++ app/src/main/java/com/maloy/muzza/ui/screens/BrowseScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -15,10 +14,10 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.material3.pulltorefresh.PullToRefreshDefaults.Indicator
 import androidx.compose.material3.pulltorefresh.pullToRefresh
@@ -293,7 +292,7 @@ fun BrowseScreen(
         }
     }
 
-    TopAppBar(
+    CenterAlignedTopAppBar(
         title = { Text(title ?: "") },
         navigationIcon = {
             IconButton(

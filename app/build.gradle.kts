f562fcb5b2da18ec6dd859933aea6cf6c8bc2b54
Maloy-Android <msermavbrin69@gmail.com>
2026-08-11 15:14:53 +0300

    Feat(App):Bump to 0.7.7-49

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,8 +88,8 @@ android {
         applicationId = "com.maloy.muzza"
         minSdk = 24
         targetSdk = 36
-        versionCode = 48
-        versionName = "0.7.6"
+        versionCode = 49
+        versionName = "0.7.7"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {

e53bdda259102affdf2ddf6a0fb906e868d8799f
Maloy-Android <msermavbrin69@gmail.com>
2026-09-22 15:36:23 +0300

    Feat(YouTubeUtils):Fixed empty thumbnails

--- app/src/main/java/com/maloy/muzza/ui/utils/YouTubeUtils.kt
+++ app/src/main/java/com/maloy/muzza/ui/utils/YouTubeUtils.kt
@@ -1,50 +1,37 @@
 package com.maloy.muzza.ui.utils
 
-import timber.log.Timber
+import kotlin.math.roundToInt
 
+private val GOOGLEUSERCONTENT_SIZE_PATTERN =
+    Regex("^(https://(?:lh3|yt3)\\.googleusercontent\\.com/[^?]*?)=w(\\d+)-h(\\d+)[^?]*(\\?.*)?$")
+private val GGPHT_SIZE_PATTERN =
+    Regex("^(https://yt3\\.ggpht\\.com/[^?=]+)=(?:s\\d+|w\\d+-h\\d+)[^?]*(\\?.*)?$")
 fun String.resize(
     width: Int? = null,
     height: Int? = null,
 ): String {
     if (width == null && height == null) return this
-    val isGoogleCdn = this.contains("googleusercontent.com") || this.contains("ggpht.com")
-    val isYtimg = this.contains("i.ytimg.com")
 
-    if (isGoogleCdn) {
-        val w = width ?: height!!
-        val h = height ?: width!!
-
-        // Handle wNNN-hNNN pattern often used in path segments
-        if (this.contains(Regex("w\\d+-h\\d+"))) {
-            return this.replace(Regex("w\\d+-h\\d+"), "w$w-h$h")
+    GOOGLEUSERCONTENT_SIZE_PATTERN
+        .matchEntire(this)
+        ?.groupValues
+        ?.let { group ->
+            val originalWidth = group[2].toInt()
+            val originalHeight = group[3].toInt()
+            val query = group[4]
+            val targetWidth = width ?: ((height!!.toDouble() * originalWidth) / originalHeight).roundToInt()
+            val targetHeight = height ?: ((width!!.toDouble() * originalHeight) / originalWidth).roundToInt()
+            return "${group[1]}=w${targetWidth.coerceAtLeast(1)}-h${targetHeight.coerceAtLeast(1)}-p-l90-rj$query"
         }
 
-        // Find where parameters start. They usually start with =w, =s, or =h
-        val baseUrl = this.split("=w", "=s", "=h", limit = 2)[0]
-
-        // If it's a banner (has =w and =h) or if both dimensions were requested, use =w-h-p format for smart cropping
-        val result = if ((this.contains("=w") && this.contains("-h")) || (width != null && height != null)) {
-            "$baseUrl=w$w-h$h-p-l90-rj"
+    GGPHT_SIZE_PATTERN.matchEntire(this)?.groupValues?.let { group ->
+        val query = group[2]
+        return if (width != null && height != null) {
+            "${group[1]}=w$width-h$height-p-l90-rj$query"
         } else {
-            // Default to =s format for square-ish images
-            "$baseUrl=s$w-p-l90-rj"
-        }
-
-        Timber.d("Resizing image: $this -> $result")
-        return result
-    } else if (isYtimg) {
-        val w = width ?: height!!
-        // For ytimg, we can try to get higher quality by replacing the suffix
-        // hqdefault.jpg is 480x360, maxresdefault.jpg is 1280x720
-        return if (w > 480) {
-            this.replace("hqdefault.jpg", "maxresdefault.jpg")
-                .replace("mqdefault.jpg", "maxresdefault.jpg")
-                .replace("sddefault.jpg", "maxresdefault.jpg")
-        } else if (w > 320) {
-            this.replace("mqdefault.jpg", "hqdefault.jpg")
-        } else {
-            this
+            "${group[1]}=s${width ?: height}$query"
         }
     }
+
     return this
 }
